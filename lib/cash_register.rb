f7d7bc73ec005d2a01763c34178cbe1ffee8fc62
Dang Nguyen <danghwin@gmail.com>
2020-05-27 23:15:02 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,10 +1,10 @@
 class CashRegister 
   
- attr_accessor :discounts, :total 
+ attr_accessor :discount, :total 
  
 
  
-  def initialize(discounts) 
+  def initialize(discount = 0) 
 
     @total = 0
     
