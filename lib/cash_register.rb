bdb83309c69687a4401245f0a320c837b558dd75
Dang Nguyen <danghwin@gmail.com>
2020-05-28 00:40:08 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,6 +1,6 @@
 class CashRegister 
   
- attr_accessor :discount, :total, :item, :price, :shopping_cart, :cart 
+ attr_accessor :discount, :total, :item, :price, :shopping_cart, 
  
 
  
@@ -9,7 +9,6 @@ class CashRegister
     @total = 0
     @discount = discount if discount != 0
     @shopping_cart = []
-    @cart = []
    end 
    
    def add_item(item, price, quantity = 1)
